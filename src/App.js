8a68380c2dfde08ce4b1cdc3aba9c78b96d2927c
ankita <ankita.adling50@gmail.com>
2023-05-08 18:56:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/posts';
+import PostForm from './components/postform';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      {/* <Posts /> */}
+      <PostForm />
+    </div>
+    </div>
+  );
+}
+
+export default App;
